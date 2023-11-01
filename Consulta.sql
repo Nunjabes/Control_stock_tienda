9a59918b29d8bcf2f6f582f632fbda3264cbbec3
Nunjabes <118215626+Nunjabes@users.noreply.github.com>
2023-11-01 17:56:40 +0100

    Actualizar Consulta.sql

--- Consulta.sql
+++ Consulta.sql
@@ -6,3 +6,14 @@ SELECT city, COUNT(*) AS c
 FROM Customers
 GROUP BY city
 ORDER BY c ASC
+
+SELECT city, COUNT(*) AS c
+FROM Customers
+GROUP BY city
+HAVING COUNT(*) > 1
+ORDER BY c DESC
+
+SELECT department, AVG(salary) AS average
+FROM staff
+GROUP BY department
+ORDER BY average DESC;
