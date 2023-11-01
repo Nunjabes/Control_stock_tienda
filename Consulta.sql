54b290d525f94192849760ea64f3cf5f5571a50b
Nunjabes <118215626+Nunjabes@users.noreply.github.com>
2023-11-01 17:53:04 +0100

    Actualizar Consulta.sql

--- Consulta.sql
+++ Consulta.sql
@@ -1,3 +1,8 @@
 SELECT * FROM Products 
 WHERE fatpercentage < (SELECT AVG(fatpercentage) FROM Products)
 ORDER BY fatpercentage
+
+SELECT city, COUNT(*) AS c
+FROM Customers
+GROUP BY city
+ORDER BY c ASC
